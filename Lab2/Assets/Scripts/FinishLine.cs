c6c92959e08828dddd05ece6dd5ced1bbb236fa2
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2025-06-11 21:35:08 +0700

    finish effect, crash effect

--- Lab2/Assets/Scripts/FinishLine.cs
+++ Lab2/Assets/Scripts/FinishLine.cs
@@ -3,12 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
-    [SerializeField] private int currentLevelIndex = 0; 
+    [SerializeField] private int currentLevelIndex = 0;
+    [SerializeField] private float reload = 1.5f;
+    [SerializeField] ParticleSystem finishEffect;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            SceneManager.LoadScene(currentLevelIndex);
+            finishEffect.Play();
+            Invoke("ReloadScene", reload); 
         }
+    }
+
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene(currentLevelIndex);
     }
 }
