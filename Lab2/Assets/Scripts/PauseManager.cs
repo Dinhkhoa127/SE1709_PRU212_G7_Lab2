0594b0d14046e95908f68a26e98bd942e0bb5970
huynqse183261 <huynqse183261@fpt.edu.vn>
2025-06-17 00:53:10 +0700

    update endgame, setting, Sound, Create Pause Game

---
+++ Lab2/Assets/Scripts/PauseManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject pauseMenuUI;
+    //public TMP_Text pauseScoreText; // Kéo Text này vào Inspector
+
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+
+        //// Lấy điểm hiện tại từ GameManager và hiển thị
+        //if (pauseScoreText != null && GameManager.instance != null)
+        //{
+        //    float score = GameManager.instance.GetCurrentScore();
+        //    pauseScoreText.text = "Score: " + Mathf.FloorToInt(score);
+        //}
+    }
+}
