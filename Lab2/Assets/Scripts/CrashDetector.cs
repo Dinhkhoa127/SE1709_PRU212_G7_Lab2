3e0cba08b814899e322faee851e94b7a7ebec190
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2025-06-20 19:16:29 +0700

    add helmet item

--- Lab2/Assets/Scripts/CrashDetector.cs
+++ Lab2/Assets/Scripts/CrashDetector.cs
@@ -6,13 +6,41 @@ public class CrashDetector : MonoBehaviour
     [SerializeField] private int currentLevelIndex = 0;
     [SerializeField] private float reload = 1f;
     [SerializeField] ParticleSystem finishEffect;
+    [SerializeField] private GameObject helmet;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Ground") || collision.CompareTag("Obstacles"))
+        bool isObstacle = collision.CompareTag("Obstacles");
+        bool isGround = collision.CompareTag("Ground");
+
+        // If helmet active, handle collisions differently
+        if (helmet != null && helmet.activeSelf)
         {
+            // With obstacle when helmet is active — lose helmet, no death
+            if (isObstacle)
+            {
+                Debug.Log("Crash obstacle, with helmet - no death");
+                Destroy(collision.gameObject); // Delete obstacle
+                helmet.SetActive(false);       // Now helmet is inactive
+                return;
+            }
+
+            // with ground when helmet is active — no death
+            if (isGround)
+            {
+                Debug.Log("Crash ground, with helmet - no death");
+                return;
+            }
+            // return to disable death logic if helmet is active
+            return;
+        }
+
+        // if dont have helmet, both obstacle and ground collisions result in death
+        if (isObstacle || isGround)
+        {
+            Debug.Log("No helmet - Death");
             finishEffect.Play();
 
-            // Tìm Rigidbody2D của player và khóa lại
             Rigidbody2D rb = GetComponent<Rigidbody2D>();
             if (rb != null)
             {
@@ -20,12 +48,11 @@ public class CrashDetector : MonoBehaviour
                 rb.bodyType = RigidbodyType2D.Static;
             }
 
-            // Nếu có script điều khiển, hãy vô hiệu hóa nó
-            // GetComponent<PlayerController>().enabled = false;
-
             Invoke("ReloadScene", reload);
         }
     }
+
+
 
     private void ReloadScene()
     {
