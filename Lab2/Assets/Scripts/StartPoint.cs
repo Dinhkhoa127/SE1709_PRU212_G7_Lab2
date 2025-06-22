d680b68b1caf8ec794a658b63435dc4342a541dc
NgBaNhan <nhan23563@gmail.com>
2025-06-22 22:02:00 +0700

    Start point

---
+++ Lab2/Assets/Scripts/StartPoint.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Star : MonoBehaviour
+{
+    [SerializeField] private float scoreValue = 15f; 
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+           
+            GameManager.Instance.AddScore(scoreValue);
+
+          
+            Destroy(gameObject);
+        }
+    }
+}
